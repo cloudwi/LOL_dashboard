92c691dbba664033112c10492a25e41935d13868
cloudwi <cloudwi@naver.com>
2023-05-10 20:11:05 +0900

    mui 적용
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 
-function App() {
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  height: 60,
+  lineHeight: "60px",
+}));
+
+const darkTheme = createTheme({ palette: { mode: "dark" } });
+const lightTheme = createTheme({ palette: { mode: "light" } });
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={2}>
+      {[lightTheme, darkTheme].map((theme, index) => (
+        <Grid item xs={6} key={index}>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                p: 2,
+                bgcolor: "background.default",
+                display: "grid",
+                gridTemplateColumns: { md: "1fr 1fr" },
+                gap: 2,
+              }}
+            >
+              {[0, 1, 2, 3, 4, 6, 8, 12, 16, 24].map((elevation) => (
+                <Item key={elevation} elevation={elevation}>
+                  {`elevation=${elevation}`}
+                </Item>
+              ))}
+            </Box>
+          </ThemeProvider>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
-
-export default App;
