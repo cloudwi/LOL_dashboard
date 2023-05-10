609416f960019677be935a2ab8d4f5177c39fcff
Ji Min Cha <97296843+d-o0o-b11@users.noreply.github.com>
2023-05-10 20:16:23 +0900

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import * as React from "react";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
-  textAlign: "center",
+  textAlign: 'center',
   color: theme.palette.text.secondary,
   height: 60,
-  lineHeight: "60px",
+  lineHeight: '60px',
 }));
 
-const darkTheme = createTheme({ palette: { mode: "dark" } });
-const lightTheme = createTheme({ palette: { mode: "light" } });
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
+const lightTheme = createTheme({ palette: { mode: 'light' } });
 
 export default function App() {
   return (
@@ -24,9 +24,9 @@ export default function App() {
             <Box
               sx={{
                 p: 2,
-                bgcolor: "background.default",
-                display: "grid",
-                gridTemplateColumns: { md: "1fr 1fr" },
+                bgcolor: 'background.default',
+                display: 'grid',
+                gridTemplateColumns: { md: '1fr 1fr' },
                 gap: 2,
               }}
             >
